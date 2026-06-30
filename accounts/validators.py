40512c361f210cb9e879c809c24232a07656c14d
Nico An <nidhish1233@gmail.com>
2026-06-30 18:26:39 +0530

    updated architecture and LMS modules

--- accounts/validators.py
+++ accounts/validators.py
@@ -1,8 +1,14 @@
 from django.core.exceptions import ValidationError
-from .utils import MAX_PROFILE_PIC_SIZE
+from .utils import MAX_PROFILE_PIC_SIZE, MAX_LECTURE_ATTACHMENT_FILE_SIZE
 
 
 def validate_image_size(image):
 
     if image.size > MAX_PROFILE_PIC_SIZE:
         raise ValidationError("Maximum size allowed for profile pic is 5MB")
+
+def validate_lecture_attachment_size(attachment):
+
+    if attachment.size > MAX_LECTURE_ATTACHMENT_FILE_SIZE:
+        raise ValidationError("Maximum size allowed for lecture attachment is 20MB")
+
--- learningManagementSystem/models.py
+++ learningManagementSystem/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from accounts.models import MyUser
+from validators import validate_lecture_attachment_size
+
 
 # Create your models here.
 
@@ -27,21 +29,37 @@ class CourseModule(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-class Content(models.Model):
+class Lecture(models.Model):
     RESOURCE_TYPES = [
         ("video", "video"),
-        ("image", "image"),
         ("blog", "blog"),
     ]
+    # which section does this lecture belong to?
     section = models.ForeignKey(CourseModule, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     duration = models.IntegerField()
-    resource = models.FileField(upload_to="/courses/content")
     order = models.IntegerField()
+    # what is the content type odf the lecrture??
     resource_type = models.CharField(choices=RESOURCE_TYPES)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+class BlogLecture(models.Model):
+    lecture = models.OneToOneField(Lecture, on_delete=models.CASCADE)
+    content_text = models.TextField()
+
+class VideoLecture(models.Model):
+    lecture = models.OneToOneField(Lecture, on_delete=models.CASCADE)
+    content_url = models.FileField(upload_to="/courses/video-content")
+
+class FileAttachment(models.Model):
+    lecture = models.ForeignKey(Lecture, on_delete=models.CASCADE)
+    content_file = models.FileField(upload_to="/courses/attachments", validators=[validate_lecture_attachment_size])
+
+class LinkAttachment(models.Model):
+    lecture = models.ForeignKey(Lecture, on_delete=models.CASCADE)
+    link = models.URLField(max_length=800)
 
 class Enrollment(models.Model):
     PROGRESS_STATUS = [
@@ -59,10 +77,10 @@ class Enrollment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-class SectionProgress(models.Model):
+class LectureProgress(models.Model):
     enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
-    # this is to track if the specific moculel is completed ot not
-    module = models.ForeignKey(CourseModule, on_delete=models.CASCADE)
+    # this is to track if the specific module is completed ot not
+    module = models.ForeignKey(Lecture, on_delete=models.CASCADE)
     is_completed = models.BooleanField(default=False)
     progress = models.DecimalField()
 
